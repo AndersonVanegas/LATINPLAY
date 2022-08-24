8c241e3c2577518dc4a253f0368a26fb2139a4db
Daniel Aguilera <da.ale.19777@gmail.com>
2022-08-23 22:59:58 -0500

    so

--- wuevo.js
+++ wuevo.js
@@ -1,1 +1,11 @@
-
+let persona = {
+    nombre: "Nombre por defecto",
+    edad: 0,
+    mascota: {
+            nombre: "Mascota por defecto",
+            edad: 0,
+    },
+};
+    
+    let juandc = JSON.parse(JSON.stringify(persona));
+    juandc.mascota.nombre = "Chacarron";
